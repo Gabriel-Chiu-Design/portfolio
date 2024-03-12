eb19a1612e18c338735f736bed983a26d62171a9
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-03-12 15:10:25 +0800

    Initial Setup

---
+++ js/web-dev.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    //$(".project").hover(function () {
+            // over
+            //$(this).find(".details").addClass("active");
+        //}, function () {
+            // out
+            //$(this).find(".details").removeClass("active");
+        //}
+    //);
+});
