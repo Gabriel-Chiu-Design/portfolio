c7b510dcbd9bc3fbc2fa6a3e7947ff168c4aad23
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-03-15 11:43:59 +0800

    Update project detail pages
    - add backtotop btn
    - update jquery CDN link

---
+++ js/general.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".header").style.top = "0";
+      } else {
+         document.querySelector(".header").style.top = "-106px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+
+    //backtotop-auto-scroll
+      $(".backtotop").click(function (e) { 
+        e.preventDefault();
+        var target=$(this).attr('href');
+        var targetPos=$(target).offset().top;
+        console.log(target, targetPos);
+        $("html, body").stop().animate({scrollTop: targetPos}, 500);
+    });
+});
