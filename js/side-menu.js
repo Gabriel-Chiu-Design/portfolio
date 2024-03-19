3912b3ad73efc37da76a5f092ebfd7614fb00797
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-03-19 14:41:44 +0800

    rwd updates on 19/3/2024

---
+++ js/side-menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".side-menu-icon").click(function (e) { 
+        e.preventDefault();
+        $(".side-menu").toggleClass("active");
+        $(".overlay").toggleClass("active");
+    });
+
+    $(".overlay").click(function (e) { 
+        e.preventDefault();
+        $(".side-menu").removeClass("active");
+        $(".overlay").removeClass("active");
+    });
+});
