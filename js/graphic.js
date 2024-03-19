654912e98698315668208f167527e0ac5b961b61
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-03-19 22:10:27 +0800

    RWD & graphics filter bar

--- js/graphic.js
+++ js/graphic.js
@@ -39,4 +39,16 @@ $(document).ready(function () {
         //turn on "All" filter
         $("#all").addClass("active");
     });
+
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".header").style.top = "0";
+        } else {
+            document.querySelector(".header").style.top = "-106px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 });
--- js/project-details.js
+++ js/project-details.js
@@ -12,11 +12,11 @@ $(document).ready(function () {
     }
 
     //backtotop-auto-scroll
-      $(".backtotop").click(function (e) { 
-        e.preventDefault();
-        var target=$(this).attr('href');
-        var targetPos=$(target).offset().top;
-        console.log(target, targetPos);
-        $("html, body").stop().animate({scrollTop: targetPos}, 500);
-    });
+    //   $(".backtotop").click(function (e) { 
+    //     e.preventDefault();
+    //     var target=$(this).attr('href');
+    //     var targetPos=$(target).offset().top;
+    //     console.log(target, targetPos);
+    //     $("html, body").stop().animate({scrollTop: targetPos}, 500);
+    // });
 });
