2e606ddf327ac063fc1ab5266d26697f540420d1
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-04-03 15:53:40 +0800

    updates on 3 Apr 2024
    - RWD adjustment on project details

--- js/project-details.js
+++ js/project-details.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".header").style.top = "0";
-      } else {
-         document.querySelector(".header").style.top = "-106px";
-      }
-      prevScrollpos = currentScrollPos;
-    }
+    // var prevScrollpos = window.pageYOffset;
+    // window.onscroll = function() {
+    //   var currentScrollPos = window.pageYOffset;
+    //   if (prevScrollpos > currentScrollPos) {
+    //     document.querySelector(".header").style.top = "0";
+    //   } else {
+    //      document.querySelector(".header").style.top = "-106px";
+    //   }
+    //   prevScrollpos = currentScrollPos;
+    // }
 
     //backtotop-auto-scroll
     //   $(".backtotop").click(function (e) { 
