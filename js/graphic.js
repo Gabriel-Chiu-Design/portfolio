6b98a4fedc1598588eb486f7b2eb126d2125825c
Gabriel Chiu <gabriel.chiu.design@gmail.com>
2024-04-09 19:28:42 +0800

    updates on 9 Apr 2024
    - content updates
    - filter adj. for graphic design gallery

--- js/graphic.js
+++ js/graphic.js
@@ -41,14 +41,14 @@ $(document).ready(function () {
     });
 
     /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".header").style.top = "0";
-        } else {
-            document.querySelector(".header").style.top = "-106px";
-        }
-        prevScrollpos = currentScrollPos;
-    }
+    // var prevScrollpos = window.pageYOffset;
+    // window.onscroll = function() {
+    //     var currentScrollPos = window.pageYOffset;
+    //     if (prevScrollpos > currentScrollPos) {
+    //     document.querySelector(".header").style.top = "0";
+    //     } else {
+    //         document.querySelector(".header").style.top = "-106px";
+    //     }
+    //     prevScrollpos = currentScrollPos;
+    // }
 });
